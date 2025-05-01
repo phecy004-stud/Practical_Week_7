168087be9aaf9e5b6d7e2fbe58800cfe45c86918
phecy004 <phecy004@ML-GP106-13>
2025-05-01 12:13:55 +0930

    Add project files.

---
+++ Practical_Week_7/Practical_Week_7.cpp
@@ -0,0 +1,12 @@
+﻿// Practical_Week_7.cpp : Defines the entry point for the application.
+//
+
+#include "Practical_Week_7.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
